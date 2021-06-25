97a7303261823c01e14469ef85cc6975b98fab4d
MIRIAM SILVA <MIRIAM-SILVA@LIVE.COM>
2021-06-24 20:39:38 -0700

    added  put

--- server.js
+++ server.js
@@ -1,56 +1,59 @@
 'use strict';
-const express = require('express');
-const app = express();
+const express = require ('express');
+const app = express ();
 
-require('dotenv').config();
+require ('dotenv').config ();
 
-const cors = require('cors');
-app.use(cors());
+const cors = require ('cors');
+app.use (cors ());
 
-app.use(express.json());
+app.use (express.json ());
 
 //identical code below
 // --------------------
-const jwt = require('jsonwebtoken');
-const jwksClient = require('jwks-rsa');
+const jwt = require ('jsonwebtoken');
+const jwksClient = require ('jwks-rsa');
 
 // this function comes directly from the jasonwebtoken docs
-const client = jwksClient({
+const client = jwksClient ({
   // this url comes from your app on the auth0 dashboard
-  jwksUri: 'https://miriamsilva.us.auth0.com/.well-known/jwks.json'
+  jwksUri: 'https://miriamsilva.us.auth0.com/.well-known/jwks.json',
 });
 
 const PORT = process.env.PORT || 3001;
 
 // this function comes directly from the jasonwebtoken docs
-function getKey(header, callback){
-  client.getSigningKey(header.kid, function(err, key) {
+function getKey (header, callback) {
+  client.getSigningKey (header.kid, function (err, key) {
     const signingKey = key.publicKey || key.rsaPublicKey;
-    callback(null, signingKey);
+    callback (null, signingKey);
   });
 }
 
 // ------------------------------
 //MongoDB things
 
-const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+const mongoose = require ('mongoose');
+mongoose.connect (process.env.DATABASE_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //copied from mongoose quickstart
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('connected to mongoDB');
+db.on ('error', console.error.bind (console, 'connection error:'));
+db.once ('open', function () {
+  console.log ('connected to mongoDB');
 });
 
-const bookSchema = new mongoose.Schema({
+const bookSchema = new mongoose.Schema ({
   name: String,
   description: String,
   status: Number,
-  email: String
+  email: String,
 });
 
-const Book = mongoose.model('Book', bookSchema);
+const Book = mongoose.model ('Book', bookSchema);
 // //this will be in models folder above
 
 // let CoolCatBook = new Book({
@@ -89,87 +92,106 @@ const Book = mongoose.model('Book', bookSchema);
 //  console.log(bookFromDB);
 // });
 
-
-app.get('/test-login', (req, res) => {
+app.get ('/test-login', (req, res) => {
   // grab the token that was sent by the frontend
-  const token = req.headers.authorization.split(' ')[1];
+  const token = req.headers.authorization.split (' ')[1];
   // make sure the token was valid
-  jwt.verify(token, getKey, {}, function(err, user) {
-    if(err) {
-      res.status(500).send('invalid token');
+  jwt.verify (token, getKey, {}, function (err, user) {
+    if (err) {
+      res.status (500).send ('invalid token');
     } else {
-      res.send(user);
+      res.send (user);
     }
   });
 });
 
-app.get('/all-books', (req, res) => {
-  Book.find({}, (err, books) => {
-    console.log(books);
-    res.send(books);
+app.get ('/all-books', (req, res) => {
+  Book.find ({}, (err, books) => {
+    console.log (books);
+    res.send (books);
   });
 });
 
 //get books
-app.get('/books', (req, res) => {
-  const token = req.headers.authorization.split(' ')[1];
+app.get ('/books', (req, res) => {
+  const token = req.headers.authorization.split (' ')[1];
   //make sure token was valid
-  jwt.verify(token, getKey, {}, function(err, user) {
+  jwt.verify (token, getKey, {}, function (err, user) {
     if (err) {
-      res.status(500).send('invalid token');
-    }
-    else {
+      res.status (500).send ('invalid token');
+    } else {
       let userEmail = user.email;
-      Book.find({email: userEmail}, (err, books) => {
-        console.log(books);
-        res.send(books);
+      Book.find ({email: userEmail}, (err, books) => {
+        console.log (books);
+        res.send (books);
       });
     }
   });
 });
 
 //post new books
-app.post('/books', (req, res)=>{
-  //get token & make sure its valid
-  const token = req.headers.authorization.split(' ')[1];
-  jwt.verify(token, getKey, {}, function(err, user) {
-    if(err) {
-      res.status(500).send('invalid token');
+app.post ('/books', (req, res) => {
+  const token = req.headers.authorization.split (' ')[1];
+  jwt.verify (token, getKey, {}, function (err, user) {
+    if (err) {
+      res.status (500).send ('invalid token');
     } else {
-      console.log(req.body);
-      const newBook = new Book({
+      console.log (req.body);
+      const newBook = new Book ({
         name: req.body.name,
         description: req.body.description,
         status: req.body.status,
-        email: user.email
+        email: user.email,
       });
-      newBook.save((err, savedBookData)=>{
-        res.send(savedBookData);
+      newBook.save ((err, savedBookData) => {
+        res.send (savedBookData);
       });
     }
   });
 });
 
 //delete books by id
-app.delete('/books/:id', (req, res)=>{
-  //get the token and verify that it works
-  const token = req.headers.authorization.split(' ')[1];
-  jwt.verify(token, getKey, {}, function(err, user) {
-    if(err) {
-      res.status(500).send('invalid token');
+app.delete ('/books/:id', (req, res) => {
+  console.log('delete', req.headers.authorization);
+  const token = req.headers.authorization.split (' ')[1];
+  jwt.verify (token, getKey, {}, function (err, user) {
+    if (err) {
+      res.status (500).send ('invalid token');
     } else {
       let bookId = req.params.id;
-      Book.deleteOne({_id: bookId, email: user.email})
-        .then(deletedBookData =>{
-          console.log(deletedBookData);
-          res.send('Your book has been deleted');
-        });
+      Book.deleteOne ({
+        _id: bookId,
+        email: user.email,
+      }).then (deletedBookData => {
+        console.log (deletedBookData);
+        res.send ('Your book has been deleted');
+      });
     }
   });
 });
 
-app.put('/books/:id', (req, res)=> {
+//update book
+app.put ('/books/:id', (req, res) => {
+  const token = req.headers.authorization.split (' ')[1];
+  jwt.verify (token, getKey, {}, function (err, user) {
+    if (err) {
+      res.status (500).send ('invalid token');
+    } else {
+      //find the book by id
+      Book.findOne({
+        _id: req.params.id,
+        email: user.email,
+      }).then(foundBook => {
+        //update
+        console.log (foundBook);
+        foundBook.name = req.body.name;
+        foundBook.description = req.body.description;
+        foundBook.status = req.body.status;
+        //send back updated book to client
+        foundBook.save().then(savedBook => res.send(savedBook));
+      });
+    }
+  });
+});
 
-}
-
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+app.listen (PORT, () => console.log (`listening on ${PORT}`));
